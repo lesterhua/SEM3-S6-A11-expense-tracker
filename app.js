9df811a2a3152d7aebfa525eab94cc6cd51cd899
lester <servqua88@gmail.com>
2019-07-04 23:21:07 +0800

    Add mongoose connection and create Record model , ignore node_modules

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Record = require("./models/record");
 
 const port = 3000;
+
+mongoose.connect("mongodb://localhost/record", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("accounting");
