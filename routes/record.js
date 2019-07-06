34f6644166a183265dd5fc6377f250e23b70138a
lester <servqua88@gmail.com>
2019-07-07 03:32:18 +0800

    Add routes of new  and action

--- routes/record.js
+++ routes/record.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Record = require("../models/record");
 
 router.get("/new", (req, res) => {
-  res.send("new page");
+  res.render("new");
 });
 
 router.post("/new", (req, res) => {
-  res.send("new action");
+  console.log("req.body", req.body);
+  const record = new Record({
+    name: req.body.name,
+    category: req.body.category,
+    date: req.body.date,
+    amount: req.body.amount
+  });
+  record.save(err => {
+    if (err) return console.error(err);
+    return res.redirect("/");
+  });
 });
 
 router.get("/:id/edit", (req, res) => {
