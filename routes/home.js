e2f0c275a8c16d77f15406553891f92a924ded73
lester <servqua88@gmail.com>
2019-07-07 01:36:17 +0800

    Add routes of home

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Record = require("../models/record");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  Record.find((err, record) => {
+    if (err) return console.error(err);
+
+    let totalAmount = 0;
+    record.filter(({ amount }) => {
+      // console.log(amount);
+      totalAmount += amount;
+    });
+    // console.log("total", totalAmount);
+    return res.render("index", { record, totalAmount });
+  });
 });
 
 module.exports = router;
