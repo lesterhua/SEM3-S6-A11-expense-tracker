5345719c4dcf894f380a930b5b492e0f8775d9fc
lester <servqua88@gmail.com>
2019-07-08 15:46:05 +0800

    Update  callbackURL

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,8 @@ module.exports = passport => {
       {
         clientID: "1251151395054525",
         clientSecret: "dcd877b1af926730e06b88eaf152dab8",
-        callbackURL: "http://localhost:3000/auth/facebook/callback",
+        callbackURL:
+          "https://glacial-savannah-63876.herokuapp.com/auth/facebook/callback",
         profileFields: ["email", "displayName"]
       },
       (accessToken, refreshToken, profile, done) => {
