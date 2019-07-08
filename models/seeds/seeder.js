a8aaa72429c8544aa903f552848a2fe3267ae3df
lester <servqua88@gmail.com>
2019-07-08 12:54:15 +0800

    Update seeder for users and records

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Record = require("../record");
+const bcrypt = require("bcryptjs");
 const spentList = require("../../spentList.json").results;
+const userList = require("../../users.json").results;
+const User = require("../user");
 
 mongoose.connect("mongodb://localhost/record", {
   useNewUrlParser: true,
@@ -16,12 +19,29 @@ db.on("error", () => {
 db.once("open", () => {
   console.log("mongodb connected!");
 
-  spentList.forEach((item, index) => {
-    Record.create({
-      name: item.name,
-      category: item.category,
-      date: item.date,
-      amount: item.amount
+  userList.forEach((item, index) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(item.password, salt, (err, hash) => {
+        const newUser = new User({
+          name: item.name,
+          email: item.email,
+          password: hash
+        });
+        newUser
+          .save()
+          .then(user => {
+            for (let i = index * 5; i < (index + 1) * 5; i++) {
+              Record.create({
+                name: spentList[i].name,
+                category: spentList[i].category,
+                date: spentList[i].date,
+                amount: spentList[i].amount,
+                userId: user._id
+              });
+            }
+          })
+          .catch(err => console.log(err));
+      });
     });
   });
   console.log("done");
